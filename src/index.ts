4b2d5b16069bcaca96c41f17635866331b87ef48
Samir Keshwani <samirkeshwani07@gmail.com>
2024-04-11 19:38:34 +0530

    added restaurant search endpoint

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
 import myRestaurantRoute from "./routes/MyRestaurantRoute";
 import { v2 as cloudinary } from "cloudinary";
+import restaurantRoute from "./routes/RestaurantRoute";
 mongoose
     .connect(process.env.MONGODB_CONNECTION_STRING as string)
     .then(() => console.log("Connected to database "));
@@ -21,7 +22,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors());
- 
+
 app.get("/health", async (req: Request,
     res: Response) => {
     res.send({
@@ -32,6 +33,7 @@ app.get("/health", async (req: Request,
 
 app.use("/api/my/user", myUserRoute);
 app.use("/api/my/restaurant", myRestaurantRoute);
+app.use("/api/restaurant", restaurantRoute);
 
 app.listen(7000, () => {
     console.log("Server started on localhost:7000");
---
+++ src/routes/RestaurantRoute.ts
@@ -0,0 +1,18 @@
+// end points for interacting with all the restaurant in the app 
+
+import express from "express";
+import { param } from "express-validator";
+import RestaurantController from "../controllers/RestaurantController";
+
+
+
+const router = express.Router();
+
+
+
+router.get("/search/:city", param("city").isString().trim().notEmpty().withMessage("City must be a valid string"), RestaurantController.searchRestaurant);
+ 
+
+
+
+export default router;
